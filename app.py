3a24bc129763220bd4563115d2fd58e06677ec39
Magnus Herold <magnus.herold@gmail.com>
2019-11-07 18:59:50 +0100

    Omaha part pretty complete

--- app.py
+++ app.py
@@ -1,28 +1,53 @@
 import json
 import connexion
 import omaha_model as omaha
+pdm = omaha.ProblemDomainModifier
+ptm = omaha.ProblemTypeModifier
+langDefault = 'EN'
 
 class Client:
-    def __init__(self, name):
+    def __init__(self, name, problems):
         self.name = name
-        self.problems = []
+        self.problems = problems
     
-    def add_problem(self, problem):
-        self.problems.append(problem)
+    def add_problem(self, *args, **kwargs):
+        self.problems.append(ProblemClassification(*args, **kwargs))
         return self
 
 class ProblemClassification:
-    def __init__(self, problem_id, comment):
+    def __init__(self, problem_id, domain_modifier, type_modifier, comment=''):
         self.problem_id = problem_id
+        self.domain_modifier = domain_modifier
+        self.type_modifier = type_modifier
+        self.ratings = []
         self.comment = comment
+
+class Rating:
+    def __init__(self, timestamp, current_state, goal_state=None):
+        self.timestamp = timestamp
+        self.current_state = current_state
+        self.goal_state = goal_state
+
+class RatingScale:
+    def __init__(self, status, knowledge, behaviour,
+            status_comment='', knowledge_comment='', behaviour_comment=''):
+        self.status = status
+        self.knowledge = knowledge
+        self.behaviour = behaviour
+        self.status_comment = status_comment
+        self.knowledge_comment = knowledge_comment
+        self.behaviour_comment = behaviour_comment
 
 def setup_examples(db):
     db['clients'] = {
-        1: Client('Emma B.')
-            .add_problem(ProblemClassification('D02P12', 'sdffdsa'))
-            .add_problem(ProblemClassification('D04P38', 'sdfsdfs')),
-        2: Client('Janice A.')
-            .add_problem(ProblemClassification('D01P01', 'sf')),
+        1: Client('Emma B.', [
+            ProblemClassification('D02P12', pdm.INDIVIDUAL, ptm.ACTUAL,
+                comment='sdffdsa'
+            ),
+            ProblemClassification('D04P38', pdm.INDIVIDUAL, ptm.ACTUAL, 'sdfsdfs'),
+        ]),
+        2: Client('Janice A.', [])
+            # .add_problem(ProblemClassification('D01P01', 'sf')),
     }
 
 ### routes
@@ -41,6 +66,7 @@ def get_client_problems(id):
     return [
         {
             'id': p.problem_id,
+            'domainModifier': p.domain_modifier.val,
             'comment': p.comment,
         }
         for p in clientdb['clients'][id].problems
@@ -49,49 +75,79 @@ def get_client_problems(id):
 def get_client_problem_interventions():
     return []
 
-def get_omaha_intervention_categories():
+def get_client_problem_ratings():
+    return []
+
+def get_omaha_intervention_categories(lang=langDefault):
     return [
         {
-            'id': p.id,
-            'title': p.title,
-            'description': p.description,
+            'id': c.id,
+            'title': c.title,
+            'description': c.description,
         }
-        for p in omahadb['intervention_categories'].values()
+        for c in omahadb[lang]['intervention_categories'].values()
     ]
 
-def get_omaha_intervention_targets():
+def get_omaha_intervention_targets(lang=langDefault):
     return [
         {
-            'id': p.id,
-            'title': p.title,
-            'description': p.description,
+            'id': t.id,
+            'title': t.title,
+            'description': t.description,
         }
-        for p in omahadb['intervention_targets'].values()
+        for t in omahadb[lang]['intervention_targets'].values()
     ]
 
-def get_omaha_problems():
+def get_omaha_problem_domains(lang=langDefault):
+    return [
+        {
+            'id': d.id,
+            'title': d.title,
+            'description': d.description,
+        }
+        for d in omahadb[lang]['domains'].values()
+    ]
+
+def get_omaha_problems(lang=langDefault):
     return [
         {
             'id': p.id,
             'title': p.title,
             'description': p.description,
             'domainId': p.domain.id,
-            'domainTitle': p.domain.title,
         }
-        for p in omahadb['problems'].values()
+        for p in omahadb[lang]['problems'].values()
     ]
 
+def get_omaha_symptoms(lang=langDefault):
+    return [
+        {
+            'id': s.id,
+            'title': s.title,
+            'domainId': s.problem.domain.id,
+            'problemId': s.problem.id,
+        }
+        for s in omahadb[lang]['symptoms'].values()
+    ]
+
+
+clientdb = {}
+omahadb = { 'EN': {}, 'DE': {} }
+omaha.setup(omahadb['EN'], 'EN')
+omaha.setup(omahadb['DE'], 'DE')
+setup_examples(clientdb)
 if __name__ == '__main__':
-    clientdb = {}
-    omahadb = {}
-    omaha.setup(omahadb)
-    setup_examples(clientdb)
+    print('------')
     print(get_clients())
     print(get_client_problems(1))
     # print(get_omaha_intervention_categories())
-    print(get_omaha_intervention_targets())
+    # print(get_omaha_intervention_targets())
     # print(get_omaha_problems())
-    print([(p.title, p.id) for p in omahadb['problems'].values() if 'Income' in p.title])
+    print(get_omaha_symptoms())
+    # print([(p.title, p.id) for p in omahadb['problems'].values() if 'Income' in p.title])
+    print(dir(omaha.ProblemDomainModifier))
+    print(dir(omaha.ProblemDomainModifier.INDIVIDUAL))
+    # print(pdm.INDIVIDUAL.title)
     print('------')
     appl = connexion.App(__name__)
     appl.add_api('openapi.yaml', validate_responses=True)
--- omaha_model.py
+++ omaha_model.py
@@ -1,30 +1,60 @@
+from enum import Enum
 import json
 
+class _Modifier(Enum):
+    @classmethod
+    def set_data(cls, data):
+        cls._data = data
+    def __init__(self, idx, val):
+        self.idx = idx
+        self.val = val
+    @property
+    def title(self):
+        return self._data[self.idx]['title']
+    def description(self):
+        return self._data[self.idx]['description']
 
-class Item:
+class ProblemDomainModifier(_Modifier):
+    INDIVIDUAL = (0, 'individual')
+    FAMILY = (1, 'family')
+    COMMUNITY = (2, 'community')
+
+class ProblemTypeModifier(_Modifier):
+    HEALTH_PROMO = (0, 'health-promotion')
+    POTENTIAL = (1, 'potential')
+    ACTUAL = (2, 'actual')
+
+class _Item:
     def __init__(self, title, description, code):
         self.title = title
         self.description = description
         self.code = code
 
-class InterventionCategory(Item):
+class InterventionCategory(_Item):
     @property
     def id(self): return 'C' + self.code
 
-class InterventionTarget(Item):
+class InterventionTarget(_Item):
     @property
     def id(self): return 'T' + self.code
 
-class ProblemDomain(Item):
+class ProblemDomain(_Item):
     @property
     def id(self): return 'D' + self.code
 
-class Problem(Item):
+class Problem(_Item):
     def __init__(self, title, description, code, domain):
         super().__init__(title, description, code)
         self.domain = domain
     @property
     def id(self): return self.domain.id + 'P' + self.code
+
+class Symptom(_Item):
+    def __init__(self, title, code, problem):
+        super().__init__(title, None, code)
+        self.problem = problem
+    @property
+    def id(self): return self.problem.id + 'S' + self.code
 
 
 def load_terminology(filename):
@@ -45,20 +75,31 @@ def parse_intervention_targets(targets):
         its[it.id] = it
     return its
 
-def parse_problems(domains):
+def parse_problems(json_domains):
+    domains = {}
     problems = {}
-    for d in domains:
+    symptoms = {}
+    for d in json_domains:
         od = ProblemDomain(d['title'], d['description'], d['code'])
+        domains[od.id] = od
         for p in d['problems']:
             op = Problem(p['title'], p['description'], p['code'], od)
             problems[op.id] = op
-    return problems
+            for s in p['signsAndSymptoms']:
+                os = Symptom(s['title'], s['code'], op)
+                symptoms[os.id] = os
+    return domains, problems, symptoms
 
-def setup(db):
-    terminology = load_terminology('terminology_EN.json')
-    db['problems'] = parse_problems(terminology['problemClassificationScheme']['domains'])
+def setup(db, lang):
+    terminology = load_terminology('terminology_' + lang + '.json')
+    db['domains'], db['problems'], db['symptoms'] =\
+        parse_problems(terminology['problemClassificationScheme']['domains'])
     db['intervention_categories'] =\
         parse_intervention_categories(terminology['interventionScheme']['categories'])
     db['intervention_targets'] =\
         parse_intervention_targets(terminology['interventionScheme']['targets'])
+    ProblemDomainModifier.set_data(
+        terminology['problemClassificationScheme']['modifiers']['scope'])
+    ProblemTypeModifier.set_data(
+        terminology['problemClassificationScheme']['modifiers']['severity'])
 
